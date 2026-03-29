4ce7017b7d0570587d269ab864ec562fbdccfdac
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-03-29 16:05:55 +0500

    Add the Dialog Component File from the ShadCN Library for the Application

---
+++ src/components/ui/dialog.tsx
@@ -0,0 +1,140 @@
+// <== IMPORTS ==>
+import {
+  forwardRef,
+  type ElementRef,
+  type HTMLAttributes,
+  type ComponentPropsWithoutRef,
+} from "react";
+import { cn } from "@/lib/utils";
+import { X } from "lucide-react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+
+// <== DIALOG ROOT ==>
+const Dialog = DialogPrimitive.Root;
+// <== DIALOG TRIGGER ==>
+const DialogTrigger = DialogPrimitive.Trigger;
+// <== DIALOG PORTAL ==>
+const DialogPortal = DialogPrimitive.Portal;
+// <== DIALOG CLOSE ==>
+const DialogClose = DialogPrimitive.Close;
+
+// <== DIALOG OVERLAY COMPONENT ==>
+const DialogOverlay = forwardRef<
+  ElementRef<typeof DialogPrimitive.Overlay>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Overlay
+    ref={ref}
+    className={cn(
+      "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      className,
+    )}
+    {...props}
+  />
+));
+// <== DISPLAY NAME FOR DEVTOOLS ==>
+DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
+
+// <== DIALOG CONTENT COMPONENT ==>
+const DialogContent = forwardRef<
+  ElementRef<typeof DialogPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      {/* CLOSE BUTTON */}
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity data-[state=open]:bg-accent data-[state=open]:text-muted-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
+        <X className="h-4 w-4" />
+        <span className="sr-only">Close</span>
+      </DialogPrimitive.Close>
+    </DialogPrimitive.Content>
+  </DialogPortal>
+));
+// <== DISPLAY NAME FOR DEVTOOLS ==>
+DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+// <== DIALOG HEADER COMPONENT ==>
+const DialogHeader = ({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col space-y-1.5 text-center sm:text-left",
+      className,
+    )}
+    {...props}
+  />
+);
+// <== DISPLAY NAME FOR DEVTOOLS ==>
+DialogHeader.displayName = "DialogHeader";
+
+// <== DIALOG FOOTER COMPONENT ==>
+const DialogFooter = ({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      className,
+    )}
+    {...props}
+  />
+);
+// <== DISPLAY NAME FOR DEVTOOLS ==>
+DialogFooter.displayName = "DialogFooter";
+
+// <== DIALOG TITLE COMPONENT ==>
+const DialogTitle = forwardRef<
+  ElementRef<typeof DialogPrimitive.Title>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Title
+    ref={ref}
+    className={cn(
+      "text-lg font-semibold leading-none tracking-tight",
+      className,
+    )}
+    {...props}
+  />
+));
+// <== DISPLAY NAME FOR DEVTOOLS ==>
+DialogTitle.displayName = DialogPrimitive.Title.displayName;
+
+// <== DIALOG DESCRIPTION COMPONENT ==>
+const DialogDescription = forwardRef<
+  ElementRef<typeof DialogPrimitive.Description>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Description
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+));
+// <== DISPLAY NAME FOR DEVTOOLS ==>
+DialogDescription.displayName = DialogPrimitive.Description.displayName;
+
+// <== EXPORTS ==>
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+};
