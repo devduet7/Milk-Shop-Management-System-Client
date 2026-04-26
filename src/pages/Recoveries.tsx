1d8f47f19e2bcd72019661d7e2fc0d914c3afc56
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-04-26 10:51:28 +0500

    Update File Paths for the Recoveries Page after Codebase Modularization

--- src/pages/Recoveries.tsx
+++ src/pages/Recoveries.tsx
@@ -45,7 +45,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { memo, useState, useCallback } from "react";
 import { Progress } from "@/components/ui/progress";
-import { PageTransition } from "@/components/PageTransition";
+import { PageTransition } from "@/components/layout/PageTransition";
 
 // <== RECOVERY INTERFACE ==>
 interface Recovery {
